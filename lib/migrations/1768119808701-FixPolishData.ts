a36dded3cb89bba1ed63da7a69dc3b1ffac73a26
Przemysław Pietrzak <przemekp95@wp.pl>
2026-01-11 09:29:11 +0100

    cleanup: remove corrupted migration file with Polish character encoding issues
    - Remove lib/migrations/1768119808701-FixPolishData.ts (had encoding issues)
    - Replaced with proper lib/migrations/1768119930671-FixPolishCharacters.ts
    - Uses individual SQL queries to avoid UTF-8 encoding problems in migration files

--- lib/migrations/1768119808701-FixPolishData.ts
+++
@@ -1,86 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class FixPolishData1768119808701 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        // Fix Polish characters and complete biographies for authors
-
-        // 1. Fix names with correct Polish characters
-        await queryRunner.query(`
-            UPDATE \`Author\`
-            SET \`name\` = CASE
-                WHEN \`slug\` = 'lempicka-wyszynska' THEN 'Dominika Aempicka-WyszyDska'
-                WHEN \`slug\` = 'rosolowski' THEN 'Marcin RosoBowski'
-                WHEN \`slug\` = 'luczuk' THEN 'Dr Piotr Auczuk'
-                WHEN \`slug\` = 'domanska' THEN 'Dr Aldona DomaDska'
-                WHEN \`slug\` = 'swietlik' THEN 'Wiktor Zwietlik'
-                WHEN \`slug\` = 'trabinski' THEN 'Piotr TrbiDski'
-                WHEN \`slug\` = 'okolowski' THEN 'Dr hab. PaweB OkoBowski'
-                WHEN \`slug\` = 'ratynski' THEN 'Dr Mateusz RatyDski'
-                WHEN \`slug\` = 'rowinski' THEN 'Tomasz RowiDski'
-                WHEN \`slug\` = 'siemiatkowski' THEN 'dr Jakub Siemitkowski'
-                WHEN \`slug\` = 'musial' THEN 'dr Adrian MusiaB'
-                WHEN \`slug\` = 'gorka' THEN 'adw. Grzegorz Górka'
-                WHEN \`slug\` = 'szymanski' THEN 'MichaB SzymaDski'
-                WHEN \`slug\` = 'masior' THEN 'dr MichaB Masior'
-                WHEN \`slug\` = 'pietrzak' THEN 'PrzemysBaw Pietrzak LL.M.'
-                ELSE \`name\`
-            END
-            WHERE \`slug\` IN (
-                'lempicka-wyszynska', 'rosolowski', 'luczuk', 'domanska',
-                'swietlik', 'trabinski', 'okolowski', 'ratynski', 'rowinski',
-                'siemiatkowski', 'musial', 'gorka', 'szymanski', 'masior', 'pietrzak'
-            )
-        `);
-
-        // 2. Update biographies with complete information
-        await queryRunner.query(`
-            UPDATE \`Author\`
-            SET \`bio\` = CASE
-                WHEN \`slug\` = 'balcerowski' THEN 'Zawodowo zwizany z trzecim sektorem. Jego zainteresowania badawcze obejmuj przede wszystkim bezpieczeDstwo publiczne i ekonomiczne. Absolwent Instytutu Nauk Politycznych Uniwersytetu Warszawskiego oraz Kolegium Gospodarki Zwiatowej SGH. Stypendysta na Wydziale Zarzdzania Uniwersytetu im. Radbouda w Holandii. Absolwent Executive MBA University of Quebec at Montreal. WykBadowca; spoBecznik; m.in. wolontariusz Fundacji im. Cichociemnych Spadochroniarzy AK; z któr jest rodzinnie zwizany.'
-                WHEN \`slug\` = 'kochman' THEN 'Absolwent WydziaBu Prawa i Administracji Uniwersytetu Warszawskiego. Adwokat  czBonek Izby Adwokackiej w Warszawie. Zawodowo od 5 lat zwizany z sektorem administracji publicznej. ZdobywaB do[wiadczenie równie| jako prawnik w podmiotach gospodarczych i kancelariach prawnych. Autor analiz i publikacji prawnych i ekonomicznych. DziaBacz spoBeczny realizujcy od kilku lat szereg projektów w trzecim sektorze. GBówny obszar badawczy: rynek finansowy w wymiarze prawnym i ekonomicznym; analiza skutków regulacji; administracja publiczna; badania i analiza postaw spoBecznych i politycznych.'
-                WHEN \`slug\` = 'rosolowski' THEN 'Absolwent WydziaBu Prawa i Administracji Uniwersytetu Warszawskiego; w latach 2006-2008 zastpca dyrektora Biura Prasowego Kancelarii Prezydenta RP  wspóBautor Pocztu przedsibiorców polskich. Wiceprezes Fundacji im. XBW Ignacego Krasickiego; czBonek Rady Fundacji Instytut Staszica.'
-                WHEN \`slug\` = 'luczuk' THEN 'Medioznawca; publicysta; ekspert ds. cyberbezpieczeDstwa. Adiunkt w Katedrze Internetu i Komunikacji Cyfrowej Instytutu Edukacji Medialnej i Dziennikarstwa UKSW. W pracy naukowo-badawczej zajmuje si równie| kwesti wizerunku i marketingu politycznego oraz zjawiskami dotyczcymi wpBywu nowoczesnych technologii na komunikacj spoBeczn. W wydawnictwie BiaBy Kruk ukazaB si debiut ksi|kowy Cyberwojna. Wojna bez amunicji?". Obszar zainteresowaD: cyberbezpieczeDstwo; rozwój rynku medialnego; wojna informacyjna i dezinformacja; wizerunek i marketing polityczny; nowe technologie.'
-                WHEN \`slug\` = 'domanska' THEN 'Prezes Instytutu Staszica; adiunkt Instytutu Studiów Midzynarodowych SzkoBy GBównej Handlowej w Warszawie; doktor habilitowany nauk ekonomicznych. Zainteresowania badawcze: Makroekonomia gospodarki otwartej; w szczególno[ci zagadnienia polityki gospodarczej  jej skuteczno[ci i uwarunkowaD w gospodarkach otwartych; znaczenie midzynarodowych wspóBzale|no[ci dla efektywno[ci polityki fiskalnej paDstwa; polityka gospodarcza a udziaB kraju w midzynarodowych ugrupowaniach integracyjnych; rozprzestrzenianie si kryzysów regionalnych i globalnych.'
-                WHEN \`slug\` = 'lewandowski' THEN 'Adwokat; doktor nauk prawnych; Dyrektor Centrum Interwencji Procesowej Ordo Iuris. Absolwent studiów prawniczych na Wydziale Prawa i Administracji Uniwersytetu Warszawskiego; które ukoDczyB z wyró|nieniem w 2012 r. Autor publikacji z zakresu prawa karnego materialnego i procesowego; historii prawa oraz teorii i filozofii prawa; publikowanych w presti|owych ogólnopolskich oraz midzynarodowych periodykach naukowych. Od 2013 r. jest zaanga|owany w dziaBalno[ organizacji pozarzdowych.'
-                WHEN \`slug\` = 'kochan' THEN 'Jzykoznawca; medioznawca. Naukowo zajmuje si jzykiem komunikacji publicznej; wizerunkiem osób i instytucji; komunikacj kryzysow; jzykiem biznesu; przemoc jzykow; perswazj; retoryk i erystyk; narracjami; prowadzeniem debat czy postkolonialnymi aspektami dyskursu publicznego. ProwadziB badania naukowe szczególnie z zakresu jzyka biznesu; sloganów i przemocy w jzyku. ByB czBonkiem projektu badawczego Komunikowanie publiczne w Polsce  ujcie inter- i multidyscyplinarne" realizowanego w latach 20132017 przez Konsorcjum Naukowe Analiza Dyskursu.'
-                WHEN \`slug\` = 'wos' THEN 'Dziennikarz i analityk ekonomiczny publikujcy m.in. w Salonie24 i Dzienniku Gazeta Prawna. Nominowany do szeregu nagród bran|owych m.in. Nagrody im. Eugeniusza Kwiatkowskiego (przyznawanej przez Akademi Ekonomiczn w Krakowie) czy Nagrody NBP im. WBadysBawa Grabskiego. Autor licznych ksi|ek m.in. Dziecica choroba liberalizmu"; To nie jest kraj dla pracowników" czy Zimna trzydziestoletnia. Nieautoryzowana biografia polskiego kapitalizmu".'
-                WHEN \`slug\` = 'gursztyn' THEN 'Dziennikarz; publicysta; historyk. PracowaB m.in. w Radiu Plus; Telewizji Puls; telewizji Polsat; Dzienniku. Polska-Europa-Zwiat"; Rzeczpospolitej"; Uwa|am Rze"; Do Rzeczy"; Polskim Radiu RDC. Obecnie pracuje w TVP; gdzie m.in. kierowaB TVP Historia i Biurem Koordynacji Programowej. Autor ksi|ek Rzez Woli. Zbrodnia nierozliczona" i Ribbentrop-Beck. Czy pakt Polska-Niemcy byB mo|liwy". W 2017 r. odznaczony Srebrnym Krzy|em ZasBugi za zasBugi na rzecz upamitniania prawdy o najnowszej historii Polski; a w 2022 r. medalem Za zasBugi dla obronno[ci kraju". Jest |oBnierzem Wojsk Obrony Terytorialnej.'
-                WHEN \`slug\` = 'lempicka-wyszynska' THEN 'Absolwentka studiów na Wydziale Katedry Jzyków Specjalistycznych Uniwersytetu Warszawskiego (jzyk angielski i niemiecki); Studiów Podyplomowych w zakresie Stosunków Midzynarodowych i Dyplomacji (Collegium Civitas) oraz Studiów Podyplomowych w zakresie E-marketingu na Uczelni Aazarskiego. Poetka; scenarzystka i lingwistka - wspóBzaBo|ycielka i prezes Fundacji Lampa; zajmujcej si krzewieniem warto[ci religijnych i patriotycznych poprzez sztuk. Stypendystka Ministra Kultury i Dziedzictwa Narodowego w zakresie poezji.'
-                ELSE \`bio\`
-            END
-            WHERE \`slug\` IN (
-                'balcerowski', 'kochman', 'rosolowski', 'luczuk', 'domanska',
-                'lewandowski', 'kochan', 'wos', 'gursztyn', 'lempicka-wyszynska'
-            )
-        `);
-
-        // 3. Fix analysis titles with correct Polish characters
-        await queryRunner.query(`
-            UPDATE \`Analysis\`
-            SET \`title\` = CASE
-                WHEN \`slug\` = 'wot-balcerowski' THEN 'Wojska Obrony Terytorialnej (WOT) w latach 2016-2022  geneza; perspektywy i historia kampanii dyskredytacyjnej'
-                WHEN \`slug\` = 'balcerowski-wegry' THEN 'Czy Polacy potrzebuj biaBo-czerwonego Orbana?'
-                WHEN \`slug\` = 'balcerowski-nacjonalizm' THEN 'O pojciu Nacjonalizm. Wprowadzenie. Cz[ I'
-                WHEN \`slug\` = 'kochman-artykul' THEN 'Rozwój otoczenia instytucjonalnego polityki mBodzie|owej w Polsce po 2015 roku'
-                WHEN \`slug\` = 'kochman-epbd' THEN 'WpByw nowelizacji dyrektywy w sprawie efektywno[ci energetycznej (EPBD) na sytuacj spoBeczno-gospodarcz w Polsce'
-                WHEN \`slug\` = 'rosolowski-energetyka' THEN 'Zielona zmiana w polskiej energetyce w [wietle polityki klimatycznej UE i oczekiwaD Polaków'
-                WHEN \`slug\` = 'rosolowski-atom' THEN 'Polski atom  pitna[cie lat wahaD; trzy lata dziaBaD'
-                WHEN \`slug\` = 'domanska-artykul' THEN 'Raport dotyczcy badania: "WpByw to|samo[ci wspólnotowej i wiedzy ekonomicznej na wybory konsumenckie studentów"'
-                WHEN \`slug\` = 'luczuk-artykul' THEN 'Polska suwerenno[ informacyjna a social media. Media (a)spoBeczno[ciowe i ich rola w dyskursie publicznym. Jak unikn zamknicia w baDce filtrujcej?'
-                WHEN \`slug\` = 'slad-luczuk' THEN 'Jak dBugi cyfrowy [lad po sobie zostawiamy i czym to grozi? Od kradzie|y to|samo[ci po programowanie wyborcy'
-                ELSE \`title\`
-            END
-            WHERE \`slug\` IN (
-                'wot-balcerowski', 'balcerowski-wegry', 'balcerowski-nacjonalizm',
-                'kochman-artykul', 'kochman-epbd', 'rosolowski-energetyka',
-                'rosolowski-atom', 'domanska-artykul', 'luczuk-artykul', 'slad-luczuk'
-            )
-        `);
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        // This migration fixes data, so down() is intentionally empty
-    }
-
-}
