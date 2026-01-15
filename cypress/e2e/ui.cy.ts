03437ab6212c6e237021a6889bda3126edf8f259
Przemysław Pietrzak <przemekp95@wp.pl>
2026-01-15 19:20:25 +0100

    test: add E2E test for search button visibility
    - Add Cypress test to verify search button is visible on header
    - Test checks for search-toggle class and SVG icon presence

--- cypress/e2e/ui.cy.ts
+++ cypress/e2e/ui.cy.ts
@@ -5,6 +5,12 @@ describe('UI – elementy wspólne', () => {
     cy.get('.navbar-toggle').should('exist');
   });
 
+  it('Przycisk wyszukiwania jest widoczny', () => {
+    cy.visit('/');
+    cy.get('.search-toggle').should('be.visible');
+    cy.get('.search-toggle svg').should('exist');
+  });
+
   it('Strona ma podstawowe elementy', () => {
     cy.visit('/');
     cy.get('main').should('exist');
