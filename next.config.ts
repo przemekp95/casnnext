4a18621262f71a81932f8fa4160366d00986be43
Przemysław Pietrzak <przemekp95@wp.pl>
2026-03-08 01:56:42 +0100

    Configure Next.js dev warnings

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,13 @@ const nextConfig = {
   typescript: { ignoreBuildErrors: false },
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   output: "standalone",
-  images: { unoptimized: true }, // <- naprawia 400 na /_next/image
+  turbopack: {
+    root: process.cwd(),
+  },
+  images: {
+    unoptimized: true, // <- naprawia 400 na /_next/image
+    qualities: [60, 68, 74, 75],
+  },
 } satisfies NextConfig;
 
 const withMDX = createMDX({
