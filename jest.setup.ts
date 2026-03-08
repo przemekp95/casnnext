04ae00f4b4d90d65c176166933c9e29e28fa7a7a
Przemysław Pietrzak <przemekp95@wp.pl>
2026-03-08 02:07:26 +0100

    Fix Jest stream polyfill typings

--- jest.setup.ts
+++ jest.setup.ts
@@ -15,6 +15,14 @@ type EdgeFetchPrimitives = {
   Blob: typeof Blob;
 };
 
+function defineGlobal(key: PropertyKey, value: unknown) {
+  Object.defineProperty(globalThis, key, {
+    value,
+    writable: true,
+    configurable: true,
+  });
+}
+
 // Set test environment (this is safe in jest setup)
 Object.defineProperty(process.env, 'NODE_ENV', {
   value: 'test',
@@ -22,21 +30,24 @@ Object.defineProperty(process.env, 'NODE_ENV', {
 });
 
 // Polyfills for Next.js cache functionality in tests
-if (typeof global.TextEncoder === 'undefined') {
-  global.TextEncoder = TextEncoder;
-  global.TextDecoder = TextDecoder;
+if (typeof globalThis.TextEncoder === 'undefined') {
+  defineGlobal('TextEncoder', TextEncoder);
 }
 
-if (typeof global.ReadableStream === 'undefined') {
-  global.ReadableStream = ReadableStream;
+if (typeof globalThis.TextDecoder === 'undefined') {
+  defineGlobal('TextDecoder', TextDecoder);
 }
 
-if (typeof global.WritableStream === 'undefined') {
-  global.WritableStream = WritableStream;
+if (typeof globalThis.ReadableStream === 'undefined') {
+  defineGlobal('ReadableStream', ReadableStream);
 }
 
-if (typeof global.TransformStream === 'undefined') {
-  global.TransformStream = TransformStream;
+if (typeof globalThis.WritableStream === 'undefined') {
+  defineGlobal('WritableStream', WritableStream);
+}
+
+if (typeof globalThis.TransformStream === 'undefined') {
+  defineGlobal('TransformStream', TransformStream);
 }
 
 if (
@@ -52,12 +63,12 @@ if (
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   const edgeFetch = require('next/dist/compiled/@edge-runtime/primitives/fetch') as EdgeFetchPrimitives;
 
-  if (typeof global.Headers === 'undefined') global.Headers = edgeFetch.Headers;
-  if (typeof global.Request === 'undefined') global.Request = edgeFetch.Request;
-  if (typeof global.Response === 'undefined') global.Response = edgeFetch.Response;
-  if (typeof global.FormData === 'undefined') global.FormData = edgeFetch.FormData;
-  if (typeof global.File === 'undefined') global.File = edgeFetch.File;
-  if (typeof global.Blob === 'undefined') global.Blob = edgeFetch.Blob;
+  if (typeof globalThis.Headers === 'undefined') defineGlobal('Headers', edgeFetch.Headers);
+  if (typeof globalThis.Request === 'undefined') defineGlobal('Request', edgeFetch.Request);
+  if (typeof globalThis.Response === 'undefined') defineGlobal('Response', edgeFetch.Response);
+  if (typeof globalThis.FormData === 'undefined') defineGlobal('FormData', edgeFetch.FormData);
+  if (typeof globalThis.File === 'undefined') defineGlobal('File', edgeFetch.File);
+  if (typeof globalThis.Blob === 'undefined') defineGlobal('Blob', edgeFetch.Blob);
 }
 
 // Mock fetch for unit tests
